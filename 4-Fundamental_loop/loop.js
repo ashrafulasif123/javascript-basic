deab2683b6ed586cf86cc3315acaa5c5a7b142a3
Afridul369 <afridul.dev@gmail.com>
2025-05-30 20:02:33 +0600

    string

---
+++ 4-Fundamental_loop/loop.js
@@ -0,0 +1,80 @@
+// even or odd number by for && While
+// const numbers = [25, 100, 55, 11, 9, 10]
+//while loop
+/* let i = 0;
+while(i < numbers.length){
+    if(numbers[i] % 2 == 0){
+        console.log('even number', numbers[i])
+    }
+    else{
+        console.log('odd number', numbers[i])
+    }
+    i++
+} */
+// for loop Without of
+/* for(i = 0; i < numbers.length; i++){
+    
+    if(numbers[i] % 2 == 0){
+        console.log('even number', numbers[i])
+    }
+    else{
+        console.log('odd number', numbers[i])
+    }
+} */
+
+// for loop with of
+/* for(const num of numbers){
+    if(num % 2 == 0){
+        console.log('even number', num)
+    }
+    else{
+        console.log('odd number', num)
+    }
+}
+ */
+
+// summation with while
+// array
+// const numbers = [1, 2, 3, 4]
+// let sum = 0;
+// let i = 0
+// while(i < numbers.length){
+//     sum = sum + numbers[i]
+//     console.log(sum);
+//     i++;
+// }
+// console.log(sum)
+// Not array
+/* let num = 0;
+let sum = 0;
+while(num < 10){
+    sum = sum + num;
+    console.log(sum)
+    num++
+}
+console.log(sum)
+ */
+
+// Decrement
+/* for(i = 10; i >= 0; i--){
+    console.log(i)
+}
+ */
+
+/* let i = 10;
+while(i < 10){
+    console.log(i)
+    i++;
+} 
+console.log(i) */
+
+/* let i;
+for(i = 10; i < 10; i++){
+    console.log(i)
+}
+console.log(i) */
+
+
+
+
+
---
+++ 4-Fundamental_loop/trim.js
@@ -0,0 +1,9 @@
+const life = 'Quran'
+const life2 = '   Quran'
+
+if(life.trim() === life2.trim()){
+    console.log('Quran is our life');
+}
+else{
+    console.log('Something is wrong')
+}
